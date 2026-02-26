b93a91aa31c2e856f351120000583cd3db7bd977
daniel florez <dricardoflorez@unicesar.edu.co>
2026-02-26 10:55:01 -0500

    feat: agregar encapsulamiento a clase Estudiante

--- src/main/java/upc/programacion3/sistemaacademicoDJD/Estudiante.java
+++ src/main/java/upc/programacion3/sistemaacademicoDJD/Estudiante.java
@@ -1,9 +1,58 @@
 package upc.programacion3.sistemaacademicoDJD;
 
 public class Estudiante {
-    String codigo;
-    String nombre;
-    String apellido;
-    int edad;
-    int semestre;
+    private String codigo;
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private int semestre;
+
+    public Estudiante(String codigo, String nombre, String apellido, int edad, int semestre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.semestre = semestre;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
+    }
 }
+
