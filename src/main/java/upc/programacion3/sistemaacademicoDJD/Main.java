68220cdef3ad51711f7690dc5b3d0f32a14b6df3
ddturizo-eng <ddturizo@unicesar.edu.co>
2026-02-25 20:27:05 -0500

    feat: crear estructura base de Main con ArrayLists

---
+++ src/main/java/upc/programacion3/sistemaacademicoDJD/Main.java
@@ -0,0 +1,20 @@
+package upc.programacion3.sistemaacademicoDJD;
+
+import java.util.ArrayList;
+
+public class Main
+{
+    private static ArrayList<Object> estudiantes = new ArrayList<>();
+    private static ArrayList<Object> asignatura = new ArrayList<>();
+    private static ArrayList<Object> notas =new ArrayList<>();
+
+    public static void main(String[] args) {
+        System.out.println("Sistema Académico DJD");
+    }
+
+    public static void mostrarMenu() {
+
+    }
+
+
+}
