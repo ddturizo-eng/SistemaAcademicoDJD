c3910edf737e19fdb6dc9318f4f8d31ac7cea8da
Jorge Herrera <jorgeiherrera@unicesar.edu.co>
2026-02-26 20:41:17 -0500

    feat: implementar DELETE - eliminar nota

--- src/main/java/upc/programacion3/sistemaacademicoDJD/Nota.java
+++ src/main/java/upc/programacion3/sistemaacademicoDJD/Nota.java
@@ -1,11 +1,16 @@
 package upc.programacion3.sistemaacademicoDJD;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Nota {
 
     private Estudiante estudiante;
     private Asignatura asignatura;
     private double valor;
     private String periodo;
+
+    private static List<Nota> listaNotas = new ArrayList<>();
 
     public Nota(Estudiante estudiante, Asignatura asignatura, double valor, String periodo) {
         this.estudiante = estudiante;
@@ -46,11 +51,58 @@ public class Nota {
         this.periodo = periodo;
     }
 
+    public static void registrarNota(Nota nota) {
+        listaNotas.add(nota);
+    }
+
+    public static List<Nota> listarNotas() {
+        return listaNotas;
+    }
+
+    public static Nota buscarNota(Estudiante estudiante, Asignatura asignatura) {
+
+        for (Nota nota : listaNotas) {
+            if (nota.getEstudiante().equals(estudiante)
+                    && nota.getAsignatura().equals(asignatura)) {
+                return nota;
+            }
+        }
+
+        return null;
+    }
+
+    public static boolean actualizarNota(Estudiante estudiante,
+                                         Asignatura asignatura,
+                                         double nuevoValor) {
+
+        Nota nota = buscarNota(estudiante, asignatura);
+
+        if (nota != null) {
+            nota.setValor(nuevoValor);
+            return true;
+        }
+
+        return false;
+    }
+
+    public static boolean eliminarNota(Estudiante estudiante,
+                                       Asignatura asignatura) {
+
+        Nota nota = buscarNota(estudiante, asignatura);
+
+        if (nota != null) {
+            listaNotas.remove(nota);
+            return true;
+        }
+
+        return false;
+    }
+
     @Override
     public String toString() {
         return "Nota{" +
-                "asignatura=" + asignatura +
-                ", estudiante=" + estudiante +
+                "estudiante=" + estudiante +
+                ", asignatura=" + asignatura +
                 ", valor=" + valor +
                 ", periodo='" + periodo + '\'' +
                 '}';
