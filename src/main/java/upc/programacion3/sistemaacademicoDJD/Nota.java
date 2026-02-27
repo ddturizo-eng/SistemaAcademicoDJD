0409eaac695c20af82ba013edf47f45a6457a377
Jorge Herrera <jorgeiherrera@unicesar.edu.co>
2026-02-26 20:21:11 -0500

    feat: agregar encapsulamiento a clase Nota

--- src/main/java/upc/programacion3/sistemaacademicoDJD/Nota.java
+++ src/main/java/upc/programacion3/sistemaacademicoDJD/Nota.java
@@ -2,8 +2,47 @@ package upc.programacion3.sistemaacademicoDJD;
 
 public class Nota {
 
-    Estudiante estudiante;
-    Asignatura asignatura;
-    double valor;
-    String periodo;
+    private Estudiante estudiante;
+    private Asignatura asignatura;
+    private double valor;
+    private String periodo;
+
+    public Nota(Estudiante estudiante, Asignatura asignatura, double valor, String periodo) {
+        this.estudiante = estudiante;
+        this.asignatura = asignatura;
+        this.valor = valor;
+        this.periodo = periodo;
+    }
+
+    public Estudiante getEstudiante() {
+        return estudiante;
+    }
+
+    public void setEstudiante(Estudiante estudiante) {
+        this.estudiante = estudiante;
+    }
+
+    public Asignatura getAsignatura() {
+        return asignatura;
+    }
+
+    public void setAsignatura(Asignatura asignatura) {
+        this.asignatura = asignatura;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getPeriodo() {
+        return periodo;
+    }
+
+    public void setPeriodo(String periodo) {
+        this.periodo = periodo;
+    }
 }
